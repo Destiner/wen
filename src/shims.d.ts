ef14552af156df67a855e2321c78190caad8f0a6
Timur Badretdinov <accounts@destiner.io>
2024-10-29 08:27:33 +0000

    fix: update declarations

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,12 +1,6 @@
-import { EIP1193Provider } from 'viem';
-
 declare module '*.vue' {
   import { ComponentOptions } from 'vue';
 
   const component: ComponentOptions;
   export default component;
-}
-
-interface Window {
-  ethereum?: EIP1193Provider;
 }
